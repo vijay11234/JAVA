974ecfb292b1ce20ad010a9faee5b1bb797ad9f8
vijay11234 <victory6923@gmail.com>
2020-09-30 00:17:37 +0530

    Using metho

---
+++ methodEmp.java
@@ -0,0 +1,37 @@
+public class methodEmp{
+   public static final int ispresent=1;
+   public static final int ispartime=2;
+   public static final int wageperhour=20;
+   public static final int num_of_wrk_days=20;
+   public static final int max_hrs_inMnth=100;
+
+   public static int computeEmpwage(){
+      int empHrs=0;
+      int totalEmpHrs=0;
+      int totalWrkDays=0;
+
+   while(totalEmpHrs <= max_hrs_inMnth && totalWrkDays < num_of_wrk_days){
+   totalWrkDays++;
+    int empCheck=(int) Math.floor(Math.random()*3);
+     switch (empCheck){
+         case ispresent:
+         empHrs=8;
+         break;
+         case ispartime:
+         empHrs=4;
+         break;
+         default:
+         empHrs=0;
+      }
+      totalEmpHrs += empHrs;
+      System.out.println("Day#: " +totalWrkDays + "Emp Hour : " +empHrs);
+   }
+   int totalEmpWage = totalEmpHrs * wageperhour;
+   System.out.println("Total empwage :" + totalEmpWage);
+	return totalEmpWage;
+}
+public static void main(String args[]){
+	computeEmpwage();
+}
+
+}
